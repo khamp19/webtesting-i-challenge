ba4dcfa4c86f58f7a22d8ebfbd4828b245a1dd3c
Kathryn Hampton <khampton119@gmail.com>
2019-04-20 13:58:20 -0400

    working except for adjusting enhancement on fail

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,54 @@ module.exports = {
   get,
 };
 
+//const item = {
+  // name: 'item-name',
+  // enhancement: 0 - 20,
+  // durability: 0 - 100
+// }
+
 function succeed(item) {
-  return { ...item };
+  // item.enhancement + 1
+  //If the item enhancement level is 20, 
+    // the enhancement level is not changed.
+  if(item.enhancement === 20){
+    return item;
+  } else {
+    let newItem = item;
+    newItem.enhancement = newItem.enhancement++;
+    return newItem;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  const newItem = item;
+  //if enhancement >= 15, durability - 10 && enhancement - 1
+  if(newItem.enhancement >= 15){
+    if(newItem.durability > 10){
+      newItem.durability = newItem.durability - 10;
+    } else {
+      newItem.durability = 0;
+    }
+    newItem.enhancement = newItem.enhancement--;
+  } else {
+    //if enhancement < 15, durability - 5
+    if(newItem.durability < 5){
+      newItem.durability = 0;
+    } else {
+      newItem.durability = newItem.durability - 5;
+    }
+  }
+  return newItem;
 }
 
 function repair(item) {
-  return { ...item };
+  //returns a new object with durability restored to 100
+  let repaired = item;
+  repaired.durability = 100;
+  return repaired;
 }
 
 function get(item) {
+  //if enhancement > 0, add [+ enhancement level to item name]
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,74 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get } = require('./enhancer.js');
 // test away!
+
+const fullEnhancement = 20;
+const fullDurability = 100;
+
+const item = {
+  name: 'sword',
+  enhancement: fullEnhancement / 2, //under 15
+  durability: fullDurability / 4, //under 50
+}
+
+const fullItem = { //full power!
+  name: 'mace',
+  enhancement: fullEnhancement,
+  durability: fullDurability
+}
+
+describe ('enhancer.js', ()=> {
+  describe('succeed()', () => {
+    it('should increase item enhancement by 1', () => {
+      expect(succeed(item).enhancement).toBe(item.enhancement++);
+      expect(succeed(fullItem).enhancement).toBe(fullEnhancement);
+    });
+    it('should not increase item enhancement past 20', () => {
+      expect(succeed(item).enhancement).toBeLessThanOrEqual(fullEnhancement);
+      expect(succeed(fullItem).enhancement).toBeLessThanOrEqual(fullEnhancement);
+    });
+  });
+
+  describe('fail()', () => {
+
+    test('enhancement < 15, reduce durability by 5 or to 0', () => {
+      const currentDur = item.durability;
+      if (item.durability > 5) {
+        expect(fail(item).durability).toBe(currentDur - 5);
+      } else {
+        expect(fail(item).durability).toBe(0);
+      }
+    });
+    test('enhancement >= 15, reduce durability by 10 ro to 0 && reduce enhancement by 1', () => {
+      const currentDur = fullItem.durability;
+      const currentEn = fullItem.enhancement;
+      if(fullItem.durability < 10){
+        expect(fail(fullItem).durability).toBe(0);
+      } else {
+        expect(fail(fullItem).durability).toBe(currentDur - 10);
+      }
+      expect(fail(fullItem).enhancement).toBe(currentEn - 1);
+    });
+    test('durability always > 0', () => {
+      expect(fail(item).durability).toBeGreaterThanOrEqual(0);
+      expect(fail(fullItem).durability).toBeGreaterThanOrEqual(0);
+    });
+    test('enhancement always > 0', () => {
+      expect(fail(item).enhancement).toBeGreaterThanOrEqual(0);
+      expect(fail(fullItem).enhancement).toBeGreaterThanOrEqual(0);
+
+    });
+  });
+
+  describe('repair()', () => {
+    it('should restore durability to 100', () => {
+      const newItem = repair(item);
+      expect(newItem.durability).toBe(100);
+      const newEnhanced = repair(fullItem);
+      expect(newEnhanced.durability).toBe(100);
+    });
+  });
+
+  // describe('get()', () => {
+
+  // })
+});
